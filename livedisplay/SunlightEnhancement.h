f28b86b74354cb9c686e240b368f0d07e4d49084
Zachx <12113971+mzakwan@users.noreply.github.com>
2019-05-14 10:32:13 +0800

    Update SunlightEnhancement.h

--- livedisplay/SunlightEnhancement.h
+++ livedisplay/SunlightEnhancement.h
@@ -1,6 +1,4 @@
 /*
- * Copyright (C) 2019 The LineageOS Project
- *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -14,15 +12,15 @@
  * limitations under the License.
  */
 
-#ifndef VENDOR_LINEAGE_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
-#define VENDOR_LINEAGE_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
+#ifndef VENDOR_AICP_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
+#define VENDOR_AICP_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
 
 #include <hidl/MQDescriptor.h>
 #include <hidl/Status.h>
-#include <vendor/lineage/livedisplay/2.0/ISunlightEnhancement.h>
+#include <vendor/aicp/livedisplay/2.0/ISunlightEnhancement.h>
 
 namespace vendor {
-namespace lineage {
+namespace aicp {
 namespace livedisplay {
 namespace V2_0 {
 namespace implementation {
@@ -35,7 +33,7 @@ class SunlightEnhancement : public ISunlightEnhancement {
   public:
     bool isSupported();
 
-    // Methods from ::vendor::lineage::livedisplay::V2_0::ISunlightEnhancement follow.
+    // Methods from ::vendor::aicp::livedisplay::V2_0::ISunlightEnhancement follow.
     Return<bool> isEnabled() override;
     Return<bool> setEnabled(bool enabled) override;
 };
@@ -43,7 +41,7 @@ class SunlightEnhancement : public ISunlightEnhancement {
 }  // namespace implementation
 }  // namespace V2_0
 }  // namespace livedisplay
-}  // namespace lineage
+}  // namespace aicp
 }  // namespace vendor
 
-#endif  // VENDOR_LINEAGE_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
+#endif  // VENDOR_AICP_LIVEDISPLAY_V2_0_SUNLIGHTENHANCEMENT_H
