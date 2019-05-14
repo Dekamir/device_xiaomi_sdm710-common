21345a735aeb875cfbd8a0271ab94c4d5297cca6
Zachx <12113971+mzakwan@users.noreply.github.com>
2019-05-14 11:16:24 +0800

    Update Android.mk

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -1,5 +1,4 @@
 LOCAL_PATH:= $(call my-dir)
-
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -9,7 +8,7 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiPocketMode
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_PRIVILEGED_MODULE := true
+LOCAL_VENDOR_MODULE := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
     android-support-v4 \
@@ -24,3 +23,5 @@ LOCAL_STATIC_JAVA_LIBRARIES := \
 LOCAL_PROGUARD_FLAG_FILES := proguard.flags
 
 include $(BUILD_PACKAGE)
+
+include $(call all-makefiles-under,$(LOCAL_PATH))
