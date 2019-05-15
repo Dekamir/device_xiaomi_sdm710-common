ab58d8bb76bcd37e46ebc693d9e2d5ee6be03444
Zachx <12113971+mzakwan@users.noreply.github.com>
2019-05-15 15:40:04 +0800

    Update Android.mk

--- pocketmode/Android.mk
+++ pocketmode/Android.mk
@@ -1,4 +1,5 @@
 LOCAL_PATH:= $(call my-dir)
+
 include $(CLEAR_VARS)
 
 LOCAL_MODULE_TAGS := optional
@@ -8,7 +9,6 @@ LOCAL_SRC_FILES := $(call all-java-files-under, src)
 LOCAL_PACKAGE_NAME := XiaomiPocketMode
 LOCAL_CERTIFICATE := platform
 LOCAL_PRIVATE_PLATFORM_APIS := true
-LOCAL_PRIVILEGED_MODULE := true
 LOCAL_VENDOR_MODULE := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
