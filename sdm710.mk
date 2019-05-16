9df90aecfee3b241fdb2e15873d39c220a2f1ffe
Zachx <12113971+mzakwan@users.noreply.github.com>
2019-05-16 14:11:07 +0800

    Update sdm710.mk

--- sdm710.mk
+++ sdm710.mk
@@ -12,7 +12,7 @@ $(call inherit-product-if-exists, vendor/xiaomi/sdm710-common/sdm710-common-vend
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
     $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay-bootleg
 
 # Properties
 -include $(LOCAL_PATH)/common-props.mk
