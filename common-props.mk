62bfee51ce56a65847eb0f2750984b3172598438
Zachx <12113971+mzakwan@users.noreply.github.com>
2019-05-16 19:35:03 +0800

    Update common-props.mk

--- common-props.mk
+++ common-props.mk
@@ -1,16 +1,51 @@
+#
+# Common vendor properties for SDM710
+#
+
 # Audio
 PRODUCT_PROPERTY_OVERRIDES += \
+    af.fast_track_multiplier=1 \
     audio.deep_buffer.media=true \
+    audio.offload.min.duration.secs=15 \
+    audio.offload.video=true \
+    persist.vendor.audio.fluence.speaker=true \
+    persist.vendor.audio.fluence.voicecall=true \
+    persist.vendor.audio.fluence.voicerec=false \
+    persist.vendor.bt.enable.splita2dp=false \
     ro.af.client_heap_size_kbyte=7168 \
     ro.config.media_vol_steps=25 \
-    ro.config.vc_call_vol_steps=7
+    ro.config.vc_call_vol_steps=7 \
+    ro.vendor.audio.sdk.fluencetype=fluence \
+    ro.vendor.audio.sdk.ssr=false \
+    vendor.audio.dolby.ds2.enabled=false \
+    vendor.audio.dolby.ds2.hardbypass=false \
+    vendor.audio_hal.period_size=192 \
+    vendor.audio.hw.aac.encoder=true \
+    vendor.audio.offload.buffer.size.kb=64 \
+    vendor.audio.offload.gapless.enabled=true \
+    vendor.audio.offload.multiaac.enable=true \
+    vendor.audio.offload.multiple.enabled=false \
+    vendor.audio.offload.passthrough=false \
+    vendor.audio.offload.track.enable=true \
+    vendor.audio.parser.ip.buffer.size=262144 \
+    vendor.audio.safx.pbe.enabled=true \
+    vendor.audio.tunnel.encode=false \
+    vendor.voice.path.for.pcm.voip=true
 
 # Bluetooth
 PRODUCT_PROPERTY_OVERRIDES += \
+    bt.max.hfpclient.connections=1 \
+    ro.bluetooth.dun=true \
+    ro.bluetooth.hfp.ver=1.7 \
+    ro.bluetooth.sap=true \
     vendor.bluetooth.soc=cherokee
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.camera.preview.ubwc=0 \
+    persist.camera.video.ubwc=0 \
+    persist.camera.gyro.disable=0 \
+    ro.persist.qcapb=1 \
     camera.disable_zsl_mode=true \
     vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,org.lineageos.snap
 
@@ -23,6 +58,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 
 # Data modules
 PRODUCT_PROPERTY_OVERRIDES += \
+    persist.data.iwlan.enable=true \
     persist.data.df.dev_name=rmnet_usb0 \
     persist.vendor.data.profile_update=true \
     persist.vendor.data.mode=concurrent \
@@ -36,15 +72,36 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.display.ad.sdr_calib_data=/vendor/etc/sdr_config.cfg \
     ro.vendor.display.sensortype=2
 
+# DRM
+PRODUCT_PROPERTY_OVERRIDES += \
+    drm.service.enabled=true
+
+# Fingerprint
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.fingerprint.cleanup.unused=false
+
 # Graphics
 PRODUCT_PROPERTY_OVERRIDES += \
     debug.sf.disable_backpressure=1 \
     debug.sf.enable_hwc_vds=1 \
     sdm.debug.disable_inline_rotator=1 \
-    sdm.debug.disable_inline_rotator_secure=1
+    sdm.debug.disable_inline_rotator_secure=1 \
+    dev.pm.dyn_samplingrate=1 \
+    persist.demo.hdmirotationlock=false \
+    persist.hwc.enable_vds=1 \
+    persist.sys.wfd.virtual=0 \
+    ro.opengles.version=196610 \
+    vendor.display.disable_rotator_downscale=1 \
+    vendor.display.disable_skip_validate=1 \
+    vendor.gralloc.enable_fb_ubwc=1 \
+    vendor.gralloc.disable_wb_ubwc=1 \
+    persist.debug.wfd.enable=1 \
+    persist.sys.wfd.nohdcp=1
 
 # Media
 PRODUCT_PROPERTY_OVERRIDES += \
+    vendor.vidc.enc.disable.pq=true \
+    vidc.enc.dcvs.extra-buff-count=2 \
     audio.offload.video=true \
     media.settings.xml=/system/etc/media_profiles_vendor.xml
 
@@ -61,14 +118,15 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.vendor.qti.core_ctl_min_cpu=4 \
     ro.vendor.qti.core_ctl_max_cpu=6 \
     vendor.iop.enable_prefetch_ofr=0 \
-    vendor.iop.enable_uxe=0
+    vendor.iop.enable_uxe=0 \
+    ro.vendor.qti.sys.fw.bg_apps_limit=60
 
 # RCS and IMS
 PRODUCT_PROPERTY_OVERRIDES += \
     persist.rcs.supported=0 \
     persist.vendor.ims.disableUserAgent=0
 
-# RIL
+# Radio
 PRODUCT_PROPERTY_OVERRIDES += \
     DEVICE_PROVISIONED=1 \
     persist.vendor.radio.atfwd.start=true \
@@ -79,12 +137,62 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ril.subscription.types=NV,RUIM \
     ro.telephony.default_network=22,22 \
     telephony.lteOnCdmaDevice=1 \
-    telephony.lteOnCdmaDevice=1 \
-    persist.dbg.ims_volte_enable=1 \
+    android.telephony.apn-restore=24000 \
+    persist.vendor.qti.telephony.vt_cam_interface=1 \
+    persist.radio.sw_mbn_update=1 \
+    persist.radio.sw_mbn_volte=1 \
+    persist.radio.sw_mbn_openmkt=1 \
+    persist.radio.hw_mbn_update=1 \
     persist.dbg.volte_avail_ovr=1 \
-    persist.dbg.vt_avail_ovr=0 \
-    persist.data.iwlan.enable=true \
-    persist.dbg.wfc_avail_ovr=0
+    persist.dbg.vt_avail_ovr=1 \
+    persist.vendor.radio.custom_ecc=1 \
+    persist.vendor.radio.process_sups_ind=1 \
+    persist.radio.multisim.config=dsds \
+    persist.vendor.radio.sw_mbn_update=1 \
+    persist.vendor.radio.sw_mbn_volte=1 \
+    persist.vendor.radio.sw_mbn_openmkt=1 \
+    persist.vendor.radio.hw_mbn_update=1 \
+    persist.radio.primarycard=true \
+    persist.radio.flexmap_type=dds \
+    persist.vendor.radio.add_power_save=1 \
+    persist.vendor.radio.rat_on=combine \
+    persist.radio.nodisplaytext=true \
+    persist.vendor.radio.ignore_dom_time=10 \
+    persist.logd.size.radio=2M \
+    persist.radio.data_ltd_sys_ind=1 \
+    persist.radio.multisim.config=dsds \
+    persist.radio.apm_sim_not_pwdn=1 \
+    persist.vendor.radio.sib16_support=1
+
+# RmNet Data
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.rmnet.data.enable=true \
+    persist.data.wda.enable=true \
+    persist.data.df.dl_mode=5 \
+    persist.data.df.ul_mode=5 \
+    persist.data.df.agg.dl_pkt=10 \
+    persist.data.df.agg.dl_size=4096 \
+    persist.data.df.mux_count=8 \
+    persist.data.df.iwlan_mux=9 \
+    persist.data.df.dev_name=rmnet_usb0
+
+# TimeService
+PRODUCT_PROPERTY_OVERRIDES += \
+    persist.timed.enable=true \
+    persist.delta_time.enable=true
+
+# USB
+PRODUCT_PROPERTY_OVERRIDES += \
+    sys.usb.controller=6a00000.dwc3 \
+    persist.vendor.usb.config.extra=none \
+    vendor.usb.dpl.inst.name=dpl \
+    vendor.usb.rmnet.func.name=rmnet_bam \
+    vendor.usb.rmnet.inst.name=rmnet \
+    vendor.usb.rndis.func.name=rndis_bam
+
+# Wifi
+PRODUCT_PROPERTY_OVERRIDES += \
+    wifi.interface=wlan0
 
 # SSR
 PRODUCT_PROPERTY_OVERRIDES += \
